b2e7ec15bfffd270e5cee4bcadeac2c069eb9573
Jason Symons <jason.symons@gmail.com>
2017-10-23 10:34:10 -0400

    Converted tests so use py.test formatting

--- test_authors.py
+++ test_authors.py
@@ -1,21 +1,24 @@
-import unittest
+
 from bookstore import Book, Author
 
 
-class AuthorTestCase(unittest.TestCase):
-    def test_author_creation(self):
-        borges = Author("Jorge Luis Borges", "AR")
-        poe = Author('Edgar Allan Poe', 'US')
+def test_author_creation():
+    borges = Author("Jorge Luis Borges", "AR")
+    poe = Author('Edgar Allan Poe', 'US')
 
-        self.assertEqual(borges.name, "Jorge Luis Borges")
-        self.assertEqual(borges.nationality, "AR")
+    assert borges.name == "Jorge Luis Borges"
+    assert borges.nationality == "AR"
 
-        self.assertEqual(poe.name, "Edgar Allan Poe")
-        self.assertEqual(poe.nationality, "US")
+    assert poe.name == "Edgar Allan Poe"
+    assert poe.nationality == "US"
 
-    def test_get_books_from_author(self):
-        borges = Author("Jorge Luis Borges", "AR")
-        ficciones = Book("Ficciones", author=borges)
+def test_get_books_from_author():
+    borges = Author("Jorge Luis Borges", "AR")
+    ficciones = Book("Ficciones", author=borges)
+    aleph = Book("The Aleph", author=borges)
+
+    assert borges.get_books() == [ficciones, aleph]
+ciones = Book("Ficciones", author=borges)
         aleph = Book("The Aleph", author=borges)
 
         self.assertEqual(borges.get_books(), [ficciones, aleph])
